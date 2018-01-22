4f4b06aee9e96df26169d60cc5ba3c698c92d777
Felix Lidforsen <felixlidforsen@>
2018-01-22 10:46:20 +0100

    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            /*int num = 7;
+            switch(num)
+            {
+                case 1:
+                    Console.WriteLine("nej b");
+                    break;
+
+                case 7:
+                    Console.WriteLine("nej bror");
+                    break;
+
+                case 8:
+                    Console.WriteLine("yes my guy!!");
+                    break;
+            }*/
+
+            var str = Console.ReadLine();
+            double num = Convert.ToDouble(str);
+
+            Console.WriteLine(num);
+        }
+    }
+}
+    
